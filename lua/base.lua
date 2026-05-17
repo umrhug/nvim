fa35fb70ed819f87837a37eea120261ceae4d68c
umrhug <umrhug@gmail.com>
2026-05-17 13:05:14 +0900

    modifies to disable loading node.js provider.

--- lua/base.lua
+++ lua/base.lua
@@ -36,6 +36,7 @@ vim.api.nvim_create_user_command(
 vim.g.python3_host_prog = vim.fn.expand(vim.fn.stdpath('data') .. '/venv/bin/python3')
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
 
 vim.g.have_nerd_font = false
 
